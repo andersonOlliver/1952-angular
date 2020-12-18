6a730cb8f16fec2470438c1bd32bc791d6e80da1
Anderson Olliver <pla.olliver@gmail.com>
2020-12-18 00:46:45 -0400

    Criando componente de transferencia

---
+++ src/app/extrato/extrato.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-extrato',
+  templateUrl: './extrato.component.html',
+  styleUrls: ['./extrato.component.scss'],
+})
+export class ExtratoComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
