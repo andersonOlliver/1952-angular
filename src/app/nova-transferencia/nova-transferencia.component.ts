aed365a684a287bd2e4fd87de55458f619b29b0e
Anderson Olliver <pla.olliver@gmail.com>
2020-12-22 10:31:59 -0400

    limpa campos ao submit

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -15,5 +15,12 @@ export class NovaTransferenciaComponent {
     console.log('Solicitada nova transferência');
     const valorEmitir = { valor: this.valor, destino: this.destino };
     this.aoTransferir.emit(valorEmitir);
+
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
   }
 }
